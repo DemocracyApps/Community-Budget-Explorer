478a4f6f24ef625daab65b26f09c7c76872d3342
ejaxon <eric@deepweave.com>
2015-02-11 15:39:24 -0500

    Just adding the Vagrant portion.

---
+++ Vagrantfile
@@ -0,0 +1,47 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+	config.vm.box = "precise64"
+	config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+	config.vm.synced_folder ".", "/var/www"
+	config.vm.network :private_network, ip: "192.168.33.23"
+
+	# Digital Ocean Provider Setup - overrides certain configuraiton options to support 
+	# a hosted setup via a Digital Ocean droplet
+	# 
+	# Install:
+	# 	You need to add a SSH private key to Digital Ocean, then specify it under 
+	# 	override.ssh.private_key_path below.
+	#
+	# Usage: 
+	# 	vagrant up --provider=digital_ocean
+    config.vm.provider :digital_ocean do |provider, override|
+    	override.vm.box = "digital_ocean"
+		override.ssh.private_key_path = "~/.ssh/id_do_rsa"
+
+		# args[0]: specifies Apache User
+		override.vm.provision :shell, :path => "setup/install.sh", :args => ["www-data"]
+
+		# This disables the private networking directive from the default config
+		override.vm.network :private_network, ip: "192.168.33.23", disabled: true
+
+		# These are the DigitalOcean provider values 
+		provider.token = ENV["DIGITAL_OCEAN_ACCESS_TOKEN"]
+		provider.image = "12.04.5 x64"
+		provider.region = "nyc2"
+	end
+
+	# PRC Start
+	# Creating a separate virtualbox configuration 
+	# For some reaosn, the provision override above wasn't working properly
+	config.vm.provider :virtualbox do |vb, override|
+		# args[0]: specifies Apache User
+		override.vm.provision :shell, :path => "setup/install.sh", :args => ["vagrant"]
+	end
+	# PRC End
+
+end
---
+++ setup/install.sh
@@ -0,0 +1,115 @@
+echo "Starting Provision"
+
+# PRC Start
+# Review arguments
+if [ "$1" != "" ]; then
+	echo "Args : $1"
+	www_user=$1
+else
+	echo "No args"
+	www_user="vagrant"
+fi
+
+echo "Apache User: $www_user"
+# End PRC 
+
+sudo apt-get update
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+sudo apt-get install -y vim curl python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php5
+sudo apt-get update
+
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug
+
+cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+xdebug.scream=1
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+EOF
+
+sudo a2enmod rewrite
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+
+sudo service apache2 restart
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# Above is from original install.sh by Jeffrey Way. 
+# The following complete the set-up for a CNP test/dev machine.
+
+sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+
+sudo rm -rf /vagrant/html
+sudo chmod -R 777 /vagrant/cnp/app/storage
+
+# Change both APACHE_RUN_USER and APACHE_RUN_GROUP to vagrant from www-data to avoid getting frequent permission-denied
+# problems with the Laravel storage subdirectory
+
+# PRC Start
+# On DigitalOcean this caused issues, as there is no vagrant user, so we make this configurable
+if [ "$www_user" != "" ]; then
+	echo "Editing Apache User" 
+	sed -i "s/www-data/$www_user/" /etc/apache2/envvars
+fi
+# End PRC
+
+if [ ! -e "/etc/apache2/sites-available/cnp.conf" ];
+    then
+
+    # PRC Revise Host Here
+    sudo cp /vagrant/FILES/cnp.conf /etc/apache2/sites-available
+    sudo a2ensite cnp
+    sudo service apache2 reload
+fi
+
+sudo apt-get install -y postgresql-client-common postgresql postgresql-contrib php5-pgsql php5-dev
+sudo apt-get install -y postgis postgresql-server-dev-9.1 postgresql-9.1-postgis
+sudo apt-get install -y postgresql-9.1-postgis-scripts
+
+#
+# In a production machine these should be limited more than here.
+#
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses='*'/" /etc/postgresql/9.1/main/postgresql.conf
+sudo sed -i "\
+99a\
+host	all	ga	0.0.0.0/0	md5" /etc/postgresql/9.1/main/pg_hba.conf
+
+sudo apt-get install -y beanstalkd supervisor
+
+# Install and configure postgres and postgis
+echo 'Setting up database and GIS extensions'
+#sudo su postgres -c 'psql -c "create user vagrant with CREATEDB PASSWORD vagrant;"'
+# sudo su postgres -c 'createuser -d -R -S vagrant'
+# sudo su postgres -c 'psql -c "ALTER USER vagrant WITH PASSWORD vagrant;"'
+sudo su postgres -c '/vagrant/SCRIPTS/create_postgres_users.sh'
+sudo su postgres -c 'createdb cnp'
+sudo su postgres -c 'psql -d cnp -c "CREATE EXTENSION postgis;"'
+sudo su postgres -c 'psql -d cnp -c "CREATE EXTENSION postgis_topology;"'
+
+# PRC Start
+# Disable the default site, since we're now serving up CNP for all Vhosts or direct IP Access
+sudo a2dissite 000-default
+
+# Update the permissions on the web root to match the apache user
+sudo chown $www_user:$www_user -R /var/www/cnp
+# End PRC
+
+sudo service apache2 restart  # Needed to load pgsql driver.
+
+sudo apt-get install -y default-jdk
+
+
+
+
+
+
+
+
