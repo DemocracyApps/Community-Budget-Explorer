8b96b838a6197538e8acf7b9c87420612d06d702
ejaxon <eric@deepweave.com>
2015-02-15 11:42:11 -0500

    Adding in all the css and such, manually for now.

--- gbe/app/Http/Controllers/HomeController.php
+++ gbe/app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller {
 	 */
 	public function __construct()
 	{
-		$this->middleware('auth');
+		//$this->middleware('auth');
 	}
 
 	/**
@@ -30,7 +30,8 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('home');
+        $projects = array();
+		return view('home', array('projects' => $projects));
 	}
 
 }
--- gbe/resources/views/home.blade.php
+++ gbe/resources/views/home.blade.php
@@ -1,17 +1,31 @@
-@extends('app')
+
+@extends('templates.default')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+    <div class="row">
+        <h1>Welcome to The Government Budget Explorer!</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in tortor ullamcorper, sodales enim quis, vehicula dui. Nulla faucibus dolor sit amet enim rhoncus rutrum. Aenean iaculis volutpat tellus, eget vulputate erat dictum ut. Nunc facilisis nisl erat, sed ornare libero lobortis at. Vestibulum eu elementum sem, nec ornare augue. Curabitur sagittis tellus at ante congue ultrices. Sed vel sagittis metus. Sed convallis, sapien eu fermentum eleifend, tortor enim consequat orci, a sagittis diam magna eu ligula. Sed dapibus facilisis nulla at tincidunt. Nulla blandit feugiat purus, a pulvinar ante. Vestibulum mollis elit ut risus facilisis, mattis venenatis metus iaculis. Fusce sed cursus sem, nec ornare erat. </p>
+        <br>
+    </div>
+    <div class="row">
+        <h2>Projects</h2>
+        <table class="table">
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+            @foreach ($projects as $project)
+                @if ($project->getProperty("access") == "Open")
+                    <tr>
+                        <td><a href="/{{$project->id}}">{{$project->name}}<a></td>
+                        <td>{{$project->description}}</td>
+                    </tr>
+                @endif
+            @endforeach
+        </table>
+    </div>
+@stop
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+@section('footer_right')
+
+@stop
