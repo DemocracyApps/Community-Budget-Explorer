89ce01ebb7a15e31d2dd326caf39ae7cff822d5c
ejaxon <eric@deepweave.com>
2015-02-16 15:46:58 -0500

    Added set up for queuing system

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,3 +10,17 @@ if cd $json_dir; then
 else
     git clone $json_repo $json_dir
 fi
+
+# Install and configure queueing system and supervisor
+echo 'Configure and start the queueing system'
+sudo sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
+sudo service beanstalkd start
+sudo cp /vagrant/setup/queue.conf /etc/supervisor/conf.d
+sudo unlink /var/run/supervisor.sock
+sudo service supervisor start
+echo 'Running supervisorctl'
+sudo supervisorctl reread
+sudo supervisorctl add queue
+sudo supervisorctl start queue
+
+echo -e "\nFinished\n"
