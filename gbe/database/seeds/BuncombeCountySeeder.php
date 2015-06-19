dd22aad6f11b581a338f51e0fbd3674e361b036a
ejaxon <eric@deepweave.com>
2015-06-19 15:45:50 -0400

    Creating the BC site

--- gbe/database/seeds/BuncombeCountySeeder.php
+++ gbe/database/seeds/BuncombeCountySeeder.php
@@ -6,7 +6,7 @@ use DemocracyApps\GB\Organizations\GovernmentOrganization;
 use DemocracyApps\GB\Users\User;
 use Illuminate\Database\Seeder;
 
-class AshevilleSeeder extends Seeder {
+class BuncombeCountySeeder extends Seeder {
 
 
     public function run()
@@ -14,13 +14,12 @@ class AshevilleSeeder extends Seeder {
         // Create the organization and first users
         $user = User::where('email','=','eric@deepweave.com')->first();
         $org = new GovernmentOrganization();
-        $org->name = "Asheville City Government";
+        $org->name = "Buncombe County, NC";
         $org->save();
         $org->addMember($user, 9);
 
         $user = User::where('email','=','admin@democracyapps.us')->first();
         $org->addMember($user, 9);
-
 
         // Create the chart of accounts
         $chart = new AccountChart();
@@ -28,42 +27,12 @@ class AshevilleSeeder extends Seeder {
         $chart->government_organization = $org->id;
         $chart->save();
 
-        \Log::info("Process accounts");
-        echo "Process accounts\n";
-        $filePath = "../sample_data/AVL2014/canonical/accounts.csv";
-        if (file_exists($filePath)) {
-            \Bus::dispatch(new \DemocracyApps\GB\Commands\DataImport\LoadAccounts($filePath, $chart->id, false));
-        }
-        else dd("No such file: " . $filePath);
-
         // Now create the categories
         $order = array();
-        \Log::info("Process funds");
-        echo "Process funds\n";
-        // Fund
-        $cat = $this->createCategory("Fund", $chart->id, "../sample_data/AVL2014/canonical/cat_fund.csv");
-        $order[] = $cat->id;
-        \Log::info("Process departments");
-        echo "Process departments\n";
-        // Department
-        $cat = $this->createCategory("Department", $chart->id, "../sample_data/AVL2014/canonical/cat_department.csv");
-        $order[] = $cat->id;
-        \Log::info("Process divisions");
-        echo "Process divisions\n";
-        // Division
-        $cat = $this->createCategory("Division", $chart->id, "../sample_data/AVL2014/canonical/cat_division.csv");
-        $order[] = $cat->id;
-        \Log::info("Process functions");
-        echo "Process functions\n";
+        \Log::info("Set up categories");
         // Function
-        $cat = $this->createCategory("Function", $chart->id, "../sample_data/AVL2014/canonical/cat_function.csv");
+        $cat = $this->createCategory("Function", $chart->id, null);
         $order[] = $cat->id;
-        \Log::info("Process cost centers");
-        echo "Process cost centers\n";
-        // Cost Center
-        $cat = $this->createCategory("Cost Center", $chart->id, "../sample_data/AVL2014/canonical/cat_costcenter.csv");
-        $order[] = $cat->id;
-
         //
         // And finally, let's load up the datasets
 
@@ -71,68 +40,19 @@ class AshevilleSeeder extends Seeder {
         echo "Process 2016 Budget\n";
         $ds = $this->createDataset("2016 Budget", "budget", Dataset::ANNUAL, 2016, null, null,
             $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/2016ProposedBudget_processed.csv", "all");
+        $ds->loadAllInOneCSVData("../sample_data/BuncombeCounty/Budget2016.csv");
 
         \Log::info("Process 2015 Budget");
         echo "Process 2015 Budget\n";
         $ds = $this->createDataset("2015 Budget", "budget", Dataset::ANNUAL, 2015, null, null,
             $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/2015AdoptedBudget_processed.csv", "all");
+        $ds->loadAllInOneCSVData("../sample_data/BuncombeCounty/Budget2015.csv");
 
-        \Log::info("Process 2014 Actuals");
-        echo "Process 2014 Actuals\n";
-        $ds = $this->createDataset("2014 Actuals", "actual", Dataset::ANNUAL, 2014, null, null,
+        \Log::info("Process 2014 Budget");
+        echo "Process 2014 Budget\n";
+        $ds = $this->createDataset("2014 Budget", "budget", Dataset::ANNUAL, 2014, null, null,
             $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/2014All_processed.csv", "all");
-
-        \Log::info("Process 2013 Actuals");
-        echo "Process 2013 Actuals\n";
-        $ds = $this->createDataset("2013 Actuals", "actual", Dataset::ANNUAL, 2013, null, null,
-                                   $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/general_fund_2013_processed.csv", "general");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/civiccenter_fund_2013_processed.csv", "civiccenter");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/masstransit_fund_2013_processed.csv", "masstransit");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/parkingservices_fund_2013_processed.csv", "parking");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/stormwater_fund_2013_processed.csv", "stormwater");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/streetcut_fund_2013_processed.csv", "streetcut");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/water_fund_2013_processed.csv", "water");
-
-        \Log::info("Process 2012 Actuals");
-        echo "Process 2012 Actuals\n";
-        $ds = $this->createDataset("2012 Actuals", "actual", Dataset::ANNUAL, 2012, null, null,
-            $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/general_fund_2012_processed.csv", "general");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/civiccenter_fund_2012_processed.csv", "civiccenter");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/masstransit_fund_2012_processed.csv", "masstransit");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/parkingservices_fund_2012_processed.csv", "parking");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/stormwater_fund_2012_processed.csv", "stormwater");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/streetcut_fund_2012_processed.csv", "streetcut");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/water_fund_2012_processed.csv", "water");
-
-        \Log::info("Process 2011 Actuals");
-        echo "Process 2011 Actuals\n";
-        $ds = $this->createDataset("2011 Actuals", "actual", Dataset::ANNUAL, 2011, null, null,
-            $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/general_fund_2011_processed.csv", "general");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/civiccenter_fund_2011_processed.csv", "civiccenter");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/masstransit_fund_2011_processed.csv", "masstransit");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/parkingservices_fund_2011_processed.csv", "parking");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/stormwater_fund_2011_processed.csv", "stormwater");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/streetcut_fund_2011_processed.csv", "streetcut");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/water_fund_2011_processed.csv", "water");
-
-        \Log::info("Process 2010 Actuals");
-        echo "Process 2010 Actuals\n";
-        $ds = $this->createDataset("2010 Actuals", "actual", Dataset::ANNUAL, 2010, null, null,
-            $org->id, $chart->id, $order);
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/general_fund_2010_processed.csv", "general");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/civiccenter_fund_2010_processed.csv", "civiccenter");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/masstransit_fund_2010_processed.csv", "masstransit");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/parkingservices_fund_2010_processed.csv", "parking");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/stormwater_fund_2010_processed.csv", "stormwater");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/streetcut_fund_2010_processed.csv", "streetcut");
-        $ds->loadCSVData("../sample_data/AVL2014/canonical/water_fund_2010_processed.csv", "water");
-
+        $ds->loadAllInOneCSVData("../sample_data/BuncombeCounty/Budget2015.csv");
 
     }
 
@@ -147,7 +67,7 @@ class AshevilleSeeder extends Seeder {
         $d->day = $day;
         $d->government_organization = $orgId;
         $d->chart = $chartId;
-        $d->category_order = json_encode($order);
+        if ($order != null) $d->category_order = json_encode($order);
         $d->save();
         return $d;
     }
@@ -156,11 +76,11 @@ class AshevilleSeeder extends Seeder {
         $category->name = $name;
         $category->chart = $chartId;
         $category->save();
-        if (file_exists($filePath)) {
-            \Bus::dispatch(new \DemocracyApps\GB\Commands\DataImport\LoadCategory($filePath, $category->id, false));
+        if ($filePath != null) {
+            if (file_exists($filePath)) {
+                \Bus::dispatch(new \DemocracyApps\GB\Commands\DataImport\LoadCategory($filePath, $category->id, false));
+            } else throw new \Exception ("BuncombeCountySeeder createCategory $name - no such file: " . $filePath);
         }
-        else throw new \Exception ("AshevilleSeeder createCategory $name - no such file: " . $filePath);
-
         return $category;
     }
 
