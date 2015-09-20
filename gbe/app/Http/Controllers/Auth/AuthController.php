555f24c2347d3df7b46b2ee89fb73462c2eeac6e
ejaxon <eric@deepweave.com>
2015-09-20 14:58:56 -0400

    Upgraded to 5.1

--- gbe/app/Http/Controllers/Auth/AuthController.php
+++ gbe/app/Http/Controllers/Auth/AuthController.php
@@ -47,13 +47,9 @@ class AuthController extends Controller
 	/**
 	 * Create a new authentication controller instance.
 	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar $registrar
 	 */
-	public function __construct(Guard $auth, Registrar $registrar)
+	public function __construct()
 	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
 
 		//$this->middleware('guest', ['except' => 'getLogout']);
 	}
