009b598f9bc363a53074de5893da41c15b323fd5
ejaxon <eric@deepweave.com>
2016-01-04 13:21:48 -0500

    Added a PUT routine to the Curl utilities

--- app/Utility/CurlUtilities.php
+++ app/Utility/CurlUtilities.php
@@ -8,11 +8,24 @@ class CurlUtilities
   {
 
     $session = curl_init($url);
-//    curl_setopt($session, CURLOPT_HEADER, false);
-//    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-//    curl_setopt($session, CURLOPT_POST, 1);
-//    curl_setopt($session, CURLOPT_POSTFIELDS, $jsonContent);
     curl_setopt($session, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($session, CURLOPT_POSTFIELDS, $jsonContent);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($session, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($session, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($jsonContent))
+    );
+
+    $returnValue = curl_exec($session);
+    curl_close($session);
+    return $returnValue;
+  }
+
+  public static function curlJsonPut ($url, $jsonContent, $timeout = 0)
+  {
+    $session = curl_init($url);
+    curl_setopt($session, CURLOPT_CUSTOMREQUEST, "PUT");
     curl_setopt($session, CURLOPT_POSTFIELDS, $jsonContent);
     curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($session, CURLOPT_TIMEOUT, $timeout);
